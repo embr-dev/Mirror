874147dc023d43527fd4ea3b88b00c560fb74d19
Russell2259 <russell2259@tuta.io>
2023-02-25 18:07:19 -0700

    update security protocol

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const require = createRequire(import.meta.url);
 
 const app = express();
 const packagefile = require('./package.json');
+
 app.use(express.json());
 app.use(cors({ origin: '*' }));
+
+app.all('/', async (req, res) => {
+    res.json({ status: 'ready', version: packagefile.version, website: 'https://gh.retronetwork.ml', description: packagefile.description, repository: packagefile.repository.url.replace('git+', '').replace('.git', '') });
+});
 
 app.all('*', async (req, res, next) => {
     const auth = await fs.readFile('./auth');
@@ -18,10 +23,12 @@ app.all('*', async (req, res, next) => {
             fetch(`http://${req.query.hostname}/auth`)
                 .then(response => response.text())
                 .then(data => {
-                    if (data == auth) {
+                    if (data == auth && req.hostname === req.query.hostname) {
+                        res.locals.authKey = data;
+
                         next();
                     } else {
-                        res.json({ error: true, errorMsg: `The domain ${req.query.hostname} does not contain the the auth token needed to access this server.` });
+                        res.json({ error: true, errorMsg: `The domain ${req.hostname} does not contain the the auth token needed to access tauthKeys server.` });
                     }
                 })
                 .catch(e => res.json({ error: true, errorMsg: `The domain ${req.query.hostname} is either not a GameHub instance or it is missing the verification token. If you are using an older fork of GameHub you may want to update.` }));
@@ -29,20 +36,23 @@ app.all('*', async (req, res, next) => {
             res.json({ error: true, errorMsg: `Missing required parameters.` })
         }
     } else {
+        res.locals.authKey = 'public';
         next();
     }
 })
 
-app.all('/', async (req, res) => {
-    res.json({ status: 'ready', version: packagefile.version, website: 'https://gh.retronetwork.ml', description: packagefile.description, repository: packagefile.repository.url.replace('git+', '').replace('.git', '') });
-});
-
 app.all('*', async (req, res) => {
     try {
+        const modifiedHeaders = req.headers;
+        modifiedHeaders.host = req.get('host');
+        modifiedHeaders.origin = `${req.protocol}://${req.get('host')}`;
+        modifiedHeaders.auth = await fs.readFile('./auth');
+        modifiedHeaders.instance = JSON.stringify({ domain: req.hostname, auth: res.locals.authKey });
+
         if (req.method == 'GET' || req.method == 'HEAD') {
-            const file = await fetch(`http://api.retronetwork.ml/GameHub${req.originalUrl}`, {
+            const file = await fetch(`http://10.82.7.62:3000/GameHub${req.originalUrl}`, {
                 method: req.method,
-                headers: req.headers
+                headers: modifiedHeaders
             });
 
             const data = new Buffer.from(await file.arrayBuffer());
@@ -55,9 +65,9 @@ app.all('*', async (req, res) => {
 
             res.end(data);
         } else {
-            const file = await fetch(`http://api.retronetwork.ml/GameHub${req.originalUrl}`, {
+            const file = await fetch(`http://10.82.7.62:3000/GameHub${req.originalUrl}`, {
                 method: req.method,
-                headers: req.headers,
+                headers: modifiedHeaders,
                 body: JSON.stringify(req.body)
             });
 
@@ -68,7 +78,6 @@ app.all('*', async (req, res) => {
         }
     } catch (e) {
         res.sendStatus(404);
-        console.log(e);
     }
 })
 
