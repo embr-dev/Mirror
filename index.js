bbbe228c62b3ba871289d7a6299ba5ffd190f38f
Russell2259 <russell2259@tuta.io>
2023-03-31 07:40:43 -0600

    use config, fix cdn

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import express from 'express';
-import * as fs from 'fs/promises';
+import * as fs from 'fs';
 import fetch from 'node-fetch';
 import cors from 'cors';
 
 const app = express();
-const games = [];
-const thumbnails = [];
+const config = JSON.parse(fs.readFileSync('./config.json', {
+    encoding: 'utf8'
+}));
+
+let games = [];
+let thumbnails = [];
 
 app.use(express.json());
 app.use(cors({ origin: '*' }));
 
-app.all('/', async (req, res) => {
-    const packageFile = JSON.parse(await fs.readFile('./package.json'));
+app.all('/', (req, res) => {
+    const packageFile = JSON.parse(fs.readFileSync('./package.json'));
     res.json({
         status: 'ready',
         version: packageFile.version,
@@ -21,15 +25,13 @@ app.all('/', async (req, res) => {
     });
 });
 
-app.all('*', async (req, res, next) => {
-    const auth = await fs.readFile('./auth');
-
-    if (auth != 'public') {
+app.all('*', (req, res, next) => {
+    if (config.auth != 'public') {
         if (req.query.hostname) {
             fetch(`http://${req.query.hostname}/auth`)
                 .then(response => response.text())
                 .then(data => {
-                    if (data == auth && req.hostname === req.query.hostname) {
+                    if (data == config.auth && req.hostname === req.query.hostname) {
                         res.locals.authKey = data;
 
                         next();
@@ -48,30 +50,34 @@ app.all('*', async (req, res, next) => {
 })
 
 app.all('/cdn/games/:id/*', async (req, res) => {
-    const gameIds = [];
-    games.forEach(game => {
-        gameIds.push(game.id);
-    })
+    if (config.gameProxy) {
+        const gameIds = [];
+        games.forEach(game => {
+            gameIds.push(game.id);
+        })
 
-    try {
-        const file = await fetch(games[gameIds.indexOf(`${req.params.id}`)].url + req.path.replace(`/cdn/games/${req.params.id}`, ''));
+        try {
+            const file = await fetch(games[gameIds.indexOf(`${req.params.id}`)].url + req.path.replace(`/cdn/games/${req.params.id}`, ''));
 
-        const data = new Buffer.from(await file.arrayBuffer());
+            const data = new Buffer.from(await file.arrayBuffer());
 
-        if (file.status !== 404) {
-            res.writeHead(file.status, { 'Content-Type': file.headers.get('content-type').split(';')[0] })
-            res.end(data);
-        } else {
-            return res.sendStatus(404);
+            if (file.status !== 404) {
+                res.writeHead(file.status, { 'Content-Type': file.headers.get('content-type').split(';')[0] })
+                res.end(data);
+            } else {
+                return res.sendStatus(404);
+            }
+        } catch (e) {
+            res.sendStatus(404);
         }
-    } catch (e) {
-        res.sendStatus(404);
+    } else {
+        res.send('A game proxy has not been enabled on this server');
     }
 });
 
 app.all('/cdn/thumbnails/:id', async (req, res) => {
     try {
-        const file = await fetch(thumbnails[req.params.id-1]);
+        const file = await fetch(thumbnails[req.params.id - 1]);
 
         const data = new Buffer.from(await file.arrayBuffer());
 
@@ -91,7 +97,8 @@ app.all('*', async (req, res, next) => {
         const modifiedHeaders = req.headers;
         modifiedHeaders.host = req.get('host');
         modifiedHeaders.origin = `${req.protocol}://${req.get('host')}`;
-        modifiedHeaders.auth = await fs.readFile('./auth');
+        modifiedHeaders.auth = config.auth;
+        modifiedHeaders.gameProxy = config.gameProxy;
         if (req.get('host').replace(':' + server.address().port, '') === req.hostname) {
             modifiedHeaders.instance = JSON.stringify({ domain: req.hostname, auth: res.locals.authKey, protocol: req.protocol });
         }
@@ -102,42 +109,38 @@ app.all('*', async (req, res, next) => {
                 headers: modifiedHeaders
             });
 
+            if (file.headers.get('redirect_url')) {
+                res.redirect(file.headers.get('redirect_url'));
+            }
+
             if (file.status !== 404) {
                 if (req.path.replace('/' + req.path.split('\\').pop().split('/').pop(), '') === '/games') {
-                    const gameIds = [];
+                    if (config.gameProxy) {
+                        const gameIds = [];
 
-                    games.forEach(game => {
-                        gameIds.push(game.id);
-                    })
+                        games.forEach(game => {
+                            gameIds.push(game.id);
+                        })
 
-                    if (!gameIds.includes(req.path.split('\\').pop().split('/').pop())) {
-                        games.push({
-                            id: req.path.split('\\').pop().split('/').pop(),
-                            url: file.headers.get('gameUrl'),
-                            thumb: file.headers.get('thumbUrl')
-                        });
+                        if (!gameIds.includes(req.path.split('\\').pop().split('/').pop())) {
+                            games.push({
+                                id: req.path.split('\\').pop().split('/').pop(),
+                                url: file.headers.get('gameUrl'),
+                                thumb: file.headers.get('thumbUrl')
+                            });
+                        }
                     }
+                }
+
+                if (req.path == '/games') {
+                    thumbnails = JSON.parse(file.headers.get('thumbnails'));
                 }
 
                 const data = new Buffer.from(await file.arrayBuffer());
 
-                if (req.path == '/games') {
-                    const moddedData = [];
-                    JSON.parse(data.toString()).forEach(game => {
-                        thumbnails.push(game.thumbnail);
-                        moddedData.push({
-                            thumbnail: `${req.protocol}://${req.get('host')}/cdn/thumbnails/${game.id}`,
-                            name: game.name,
-                            id: game.id
-                        })
-                    });
+                res.writeHead(file.status, { 'Content-Type': file.headers.get('content-type').split(';')[0] })
 
-                    res.json(moddedData);
-                } else {
-                    res.writeHead(file.status, { 'Content-Type': file.headers.get('content-type').split(';')[0] })
-
-                    res.end(data);
-                }
+                res.end(data);
             } else {
                 next();
             }
